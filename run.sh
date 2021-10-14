9d8e0280c4afe8bf567088ebca8824be6807317a
Kenjidoom <vitodevcool@protonmail.com>
2021-10-14 13:20:39 -0700

    shell scripts

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 # run.sh command is used to execute different types of programming scripts, depending on their file extension.
 FILE="$1"
 
-if [[ $FILE = *.py ]]
-then
-	python $FILE
-else
-	echo "None"
-fi
+# PYTHON
+if [[ $FILE = *.py ]]; then python $FILE; fi 
+
+# SHELLY
+if [[ $FILE = *.sh ]]; then ./$FILE; fi
+
